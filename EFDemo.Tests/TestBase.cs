32265eef6dcbc0cf0f063d7bfbe1d9186b96ce7d
Ivan Sultanov <Ivan_Sultanov@epam.com>
2023-01-29 16:10:54 +0600

    Add run migration befor tests

--- EFDemo.Tests/TestBase.cs
+++ EFDemo.Tests/TestBase.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using EFDemo;
+using Microsoft.EntityFrameworkCore;
 
 namespace EF.Tests
 {
     public class TestBase : IDisposable
     {
         public readonly Dal Dal;
+        public EFDemoDbContext _dbContext;
 
         public TestBase()
         {
-            Dal = new Dal(new EFDemoDbContext());
+            _dbContext = new EFDemoDbContext();
+            
+            _dbContext.Database.Migrate();
+            
+            Dal = new Dal(_dbContext);
+
             Dal.ClearAllData();
         }
 
         public void Dispose()
         {
             Dal.ClearAllData();
+            _dbContext.Dispose();
         }
     }
 }
